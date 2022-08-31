67516bc2a932cde2072f39402c8bfe0d70592c26
tada3345 <tada3345@gmail.com>
2022-08-31 13:09:38 +0900

    initial commit

---
+++ swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    int x =28;
+    int y=50;
+
+    printf("x is %i\n", x);
+    printf("y is %i\n", y);
+
+//swapping
+
+
+
+    printf("x is %i\n", x);
+    printf("y is %i\n", y);
+}
